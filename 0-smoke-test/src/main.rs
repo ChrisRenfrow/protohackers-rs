bae53a6e2af65a4b014ad0284c8bec09aafa3c0d
Chris Renfrow <dev@chrisrenfrow.me>
2022-12-31 21:30:22 -0800

    Make connection-handling multi-threaded

--- 0-smoke-test/src/main.rs
+++ 0-smoke-test/src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
+
 use clap::Parser;
 use smoke_test::{cli::ServerArgs, server};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> io::Result<()> {
     let port = ServerArgs::parse().port;
 
     server::run(port).await
--- 0-smoke-test/src/server.rs
+++ 0-smoke-test/src/server.rs
@@ -5,25 +5,29 @@ use tokio::{
     net::{TcpListener, TcpStream},
 };
 
-pub async fn run(port: u32) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn run(port: u32) -> io::Result<()> {
     let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).await?;
 
     loop {
-        let (sock, _) = listener.accept().await?;
-        echo(sock).await?;
+        let (sock, addr) = listener.accept().await?;
+        println!("Accepted connection: {}", addr);
+        tokio::spawn(async move { echo(sock).await });
     }
 }
 
-async fn echo(mut socket: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+async fn echo(mut socket: TcpStream) {
     let mut msg = vec![0; 1024];
 
     loop {
-        socket.readable().await?;
+        if let Err(e) = socket.readable().await {
+            eprintln!("{}", e);
+            return;
+        }
 
         match socket.try_read(&mut msg) {
             Ok(0) => {
                 println!("Info: Read zero bytes, closing connection.");
-                return Ok(());
+                return;
             }
             Ok(n) => {
                 let received = &msg[0..n];
@@ -31,14 +35,18 @@ async fn echo(mut socket: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
                     "Received message: {}\nEchoing...",
                     String::from_utf8(received.to_vec()).unwrap()
                 );
-                socket.write_all(received).await?;
+                if let Err(e) = socket.write_all(received).await {
+                    eprintln!("{}", e);
+                    return;
+                }
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 println!("Info: No more bytes to read");
                 continue;
             }
             Err(e) => {
-                return Err(e.into());
+                eprintln!("{}", e);
+                return;
             }
         }
     }
